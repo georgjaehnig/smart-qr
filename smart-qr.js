5d0c64d51618fe7979b635ab8928ac32ad1e7411
Georg Jaehnig <georg@jaehnig.org>
2017-04-17 00:39:16 +0200

    Wikipedia locations.

--- smart-qr.js
+++ smart-qr.js
@@ -9,10 +9,10 @@
 
 var i;
 
-function addQR(event) {
+function addQR(value) {
+	console.log(encodeURIComponent(value));
 	i = document.createElement("img");
-	//console.log(encodeURIComponent(event.target.smartQRvalue));
-	i.src = "https://qrcode.kaywa.com/img.php?s=6&d=" + encodeURIComponent(event.target.smartQRvalue);
+	i.src = "https://qrcode.kaywa.com/img.php?s=6&d=" + encodeURIComponent(value);
 	i.style.width = "100px";
 	i.style.height = "100px";
 	i.style.position = "fixed";
@@ -27,9 +27,17 @@ function removeQR() {
 };
 
 function addListeners(element, value) {
-	element.addEventListener('mouseover', addQR);
-	element.addEventListener('mouseout', removeQR);
+	element.addEventListener('mouseover', function() { addQR(value); } );
+	element.addEventListener('mouseout', function() { removeQR(); });
 	element.smartQRvalue = value;
+}
+
+function handleTextSelect(event) {
+	return;
+	var selection = document.getSelection();
+	var text = selection.toString();
+	console.log(selection);
+	addListeners(selection.baseNode, text);
 }
 
 (function () {
@@ -38,16 +46,26 @@ function addListeners(element, value) {
 	for(var i=0, len=links.length; i < len; i++){
 		var link = links[i];
 		if (link.href) {
+			// Links with href="tel:..."
 			var re = new RegExp('^tel:(.*)$');
 			var matches = link.href.match(re);
 			if (matches) {
 				addListeners(link, link.href);
 			}
+			// Wikipedia location links.
+			var re = new RegExp('tools.wmflabs.org/geohack/geohack\.php.*params=([0-9\.]*)_._([0-9\.]*)');
+			var matches = link.href.match(re);
+			if (matches) {
+				var value = 'geo:' + matches[1] + ',' + matches[2];
+				addListeners(link, value);
+			}
 		}
+		// Links with data-number.
 		if (link.dataset) {
 			if (link.dataset.number) {
 				addListeners(link, link.dataset.number);
 			}
 		}
 	}
+	// document.addEventListener('mouseup', handleTextSelect);
 }());
