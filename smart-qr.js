4e6e5174d6437f838bf9e60ddf3ebc0fd92e0bef
Georg Jaehnig <georg@jaehnig.org>
2017-04-17 14:09:45 +0200

    Refactor.

--- smart-qr.js
+++ smart-qr.js
@@ -10,7 +10,6 @@
 
 (function () {
 
-
 	function addQR(value) {
 		console.log(value);
 		i.src = "https://qrcode.kaywa.com/img.php?s=6&d=" + encodeURIComponent(value);
@@ -42,41 +41,43 @@
 		}
 	}
 
-  "use strict";
-  var links = document.getElementsByTagName('a');
-  for(var i=0, len=links.length; i < len; i++){
-    var link = links[i];
-    if (link.href) {
-      // Links with href="tel:..."
-      var re = new RegExp('^tel:(.*)$');
-      var matches = link.href.match(re);
-      if (matches) {
-        addListeners(link, link.href);
-      }
-      // Links with href="maps.google.com/?.*q=12,34"
-      var re = new RegExp('maps.google.com.*q=([0-9\.]*),([0-9\.]*)');
-      var matches = link.href.match(re);
-      if (matches) {
-        var value = 'geo:' + matches[1] + ',' + matches[2];
-        addListeners(link, value);
-      }
-      // Wikipedia location links.
-      var re = new RegExp('tools.wmflabs.org/geohack/geohack\.php.*params=([0-9\.]*)_._([0-9\.]*)');
-      var matches = link.href.match(re);
-      if (matches) {
-        var value = 'geo:' + matches[1] + ',' + matches[2];
-        addListeners(link, value);
-      }
-    }
-    // Links with data-number.
-    if (link.dataset) {
-      if (link.dataset.number) {
-        addListeners(link, link.dataset.number);
-      }
-    }
-  }
+	function parseAndAddListeners() {
+		var links = document.getElementsByTagName('a');
+		for(var i=0, len=links.length; i < len; i++){
+			var link = links[i];
+			if (link.href) {
+				// Links with href="tel:..."
+				var re = new RegExp('^tel:(.*)$');
+				var matches = link.href.match(re);
+				if (matches) {
+					addListeners(link, link.href);
+				}
+				// Links with href="maps.google.com/?.*q=12,34"
+				var re = new RegExp('maps.google.com.*q=([0-9\.]*),([0-9\.]*)');
+				var matches = link.href.match(re);
+				if (matches) {
+					var value = 'geo:' + matches[1] + ',' + matches[2];
+					addListeners(link, value);
+				}
+				// Wikipedia location links.
+				var re = new RegExp('tools.wmflabs.org/geohack/geohack\.php.*params=([0-9\.]*)_._([0-9\.]*)');
+				var matches = link.href.match(re);
+				if (matches) {
+					var value = 'geo:' + matches[1] + ',' + matches[2];
+					addListeners(link, value);
+				}
+			}
+			// Links with data-number.
+			if (link.dataset) {
+				if (link.dataset.number) {
+					addListeners(link, link.dataset.number);
+				}
+			}
+		}
+		document.addEventListener('mouseup', handleTextSelect);
+	}
 
-  document.addEventListener('mouseup', handleTextSelect);
+  "use strict";
 
 	// Create code and add to body.
 	var i = document.createElement("img");
@@ -89,6 +90,8 @@
 	i.alt = "QR Code";
 
 	document.body.appendChild(i);
+
+	parseAndAddListeners();
 
 }());
 
